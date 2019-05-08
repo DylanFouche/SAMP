7cb2f4fdeb2649ac0a36bbf0fde3402f966d866e
Dylan Fouche <dylanfouche@gmail.com>
2019-05-08 19:45:25 +0200

    Wrote special member functions for audio class + tests

--- include/audio.h
+++ include/audio.h
@@ -7,4 +7,74 @@
 #ifndef _AUDIO_H
 #define _AUDIO_H
 
+#include <cstdint>
+#include <string>
+#include <vector>
+
+template<typename S, int C>
+class audio
+{
+  private:
+    int sampleRate;
+    int bitDepth;
+    std::vector<S> buffer;
+
+    void load(std::string filename);
+    void save(std::string filename);
+
+  public:
+    /*
+      Special member functions
+    */
+    audio(int sampleRate, int bitDepth, std::vector<S> buffer):
+      sampleRate(sampleRate),
+      bitDepth(bitDepth),
+      buffer(buffer){}
+
+    audio(int sampleRate, int bitDepth, std::string filename):
+      sampleRate(sampleRate),
+      bitDepth(bitDepth){
+        load(filename);
+      }
+
+    audio() = default;
+    ~audio(){buffer.clear();}
+
+    audio(const audio& lhs):
+      sampleRate(lhs.sampleRate),
+      bitDepth(lhs.bitDepth),
+      buffer(lhs.buffer){}
+
+    audio(audio&& rhs):
+      sampleRate(rhs.sampleRate),
+      bitDepth(rhs.bitDepth){
+        buffer = std::move(rhs.buffer);
+        rhs.sampleRate = 0;
+        rhs.bitDepth = 0;
+      }
+
+    audio& operator=(const audio& lhs){
+      sampleRate = lhs.sampleRate;
+      bitDepth = lhs.bitDepth;
+      buffer = lhs.buffer;
+      return *this;
+    }
+
+    audio& operator=(audio&& rhs){
+      sampleRate = rhs.sampleRate;
+      bitDepth = rhs.bitDepth;
+      buffer = std::move(rhs.buffer);
+      rhs.sampleRate = 0;
+      rhs.bitDepth = 0;
+      return *this;
+    }
+
+    /*
+      accessor methods
+    */
+    int getSampleRate() const{return sampleRate;}
+    int getBitDepth() const{return bitDepth;}
+    S& operator[](int i){return buffer.at(i);}
+};
+
 #endif
--- test/tests.cpp
+++ test/tests.cpp
@@ -8,3 +8,117 @@
 #include "catch.hpp"
 
 #include "audio.h"
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+TEST_CASE("Testing audio:audio(int sampleRate, int bitDepth, std::vector<S> buffer) constructor)")
+{
+  vector<int> v;
+  audio<int, 1> a1(44100, 16, v);
+  REQUIRE(
+    a1.getSampleRate() == 44100
+  );
+  REQUIRE(
+    a1.getBitDepth() == 16
+  );
+}
+TEST_CASE("Testing audio copy constructor")
+{
+  vector<int> v;
+  audio<int, 1> a1(44100, 16, v);
+  audio<int, 1> a2(a1);
+  REQUIRE(
+    a1.getSampleRate() == 44100
+  );
+  REQUIRE(
+    a1.getBitDepth() == 16
+  );
+  REQUIRE(
+    a2.getSampleRate() == 44100
+  );
+  REQUIRE(
+    a2.getBitDepth() == 16
+  );
+}
+TEST_CASE("Testing audio move constructor")
+{
+  vector<int> v;
+  audio<int, 1> a1(44100, 16, v);
+  audio<int, 1> a2(move(a1));
+  REQUIRE(
+    a2.getSampleRate() == 44100
+  );
+  REQUIRE(
+    a2.getBitDepth() == 16
+  );
+  REQUIRE(
+    a1.getSampleRate() == 0
+  );
+  REQUIRE(
+    a1.getBitDepth() == 0
+  );
+}
+TEST_CASE("Testing audio copy assignment operator")
+{
+  vector<int> v;
+  audio<int, 1> a1(44100, 16, v);
+  audio<int, 1> a2 = a1;
+  REQUIRE(
+    a1.getSampleRate() == 44100
+  );
+  REQUIRE(
+    a1.getBitDepth() == 16
+  );
+  REQUIRE(
+    a2.getSampleRate() == 44100
+  );
+  REQUIRE(
+    a2.getBitDepth() == 16
+  );
+}
+TEST_CASE("Testing audio move assignment operator")
+{
+  vector<int> v;
+  audio<int, 1> a1(44100, 16, v);
+  audio<int, 1> a2 = move(a1);
+  REQUIRE(
+    a2.getSampleRate() == 44100
+  );
+  REQUIRE(
+    a2.getBitDepth() == 16
+  );
+  REQUIRE(
+    a1.getSampleRate() == 0
+  );
+  REQUIRE(
+    a1.getBitDepth() == 0
+  );
+}
+TEST_CASE("Testing audio operator[]")
+{
+  vector<int> v{10,20,30};
+  audio<int, 1> a1(44100, 16, v);
+  REQUIRE(
+    a1[0]==10
+  );
+  REQUIRE(
+    a1[1]==20
+  );
+  REQUIRE(
+    a1[2]==30
+  );
+  a1[0]=40;
+  REQUIRE(
+    a1[0]==40
+  );
+  REQUIRE(
+    a1[1]==20
+  );
+  REQUIRE(
+    a1[2]==30
+  );
+}
